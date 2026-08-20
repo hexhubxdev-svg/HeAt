b7ccac4a189bf219ecd34a0ea18d1e87532616c7
hexhubxdev-svg <hexhubxdev@gmail.com>
2026-08-20 06:55:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,23 +2,22 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("ddef08b7b5e10142673bdb9467fef74055ddc718459781363589a98f36420f4a")
+TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Welcome Use /help for commands.")
+    await update.message.reply_text("Welcome Use /help for commands")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("""
 Available commands:
 /start - Start the bot
-/help - Show this message
+/help - Show this help message
 """)
 
 def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
-    print("Bot is running...")
     app.run_polling()
 
 if __name__ == "__main__":
